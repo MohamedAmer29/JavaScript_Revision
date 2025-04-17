f0eaf73792dec9fb22b4262d72004c5b5bdbe39f
Amer <m1o1h1a1a1@gmail.com>
2025-04-17 18:02:03 +0200

    used modules and import cart,products from cart.js and products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 let hmtlText = "";
 products.forEach((product) => {
   const html = `<div class="product-container">
