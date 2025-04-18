172ae97feb390ad8ee7dc7e5d4f0e39e50e93151
Amer <m1o1h1a1a1@gmail.com>
2025-04-18 21:09:14 +0200

    automated test with jasmine unit testing

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,22 @@
-export let cart = JSON.parse(localStorage.getItem("cart"));
+export let cart;
+loadFromStorage();
 let matchingItem;
-if (!cart) {
-  cart = [
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
-      deliveryOptionId: "1",
-    },
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1,
-      deliveryOptionId: "3",
-    },
-  ];
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
+  if (!cart) {
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: "3",
+      },
+    ];
+  }
 }
 export function saveToStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
@@ -20,6 +24,7 @@ export function saveToStorage() {
 
 export // add to card function
 function addToCart(productId) {
+  let matchingItem;
   const select = document.querySelector(`.js-quantity-selector-${productId}`);
   cart.forEach((cardItem) => {
     if (productId === cardItem.productId) {
@@ -40,7 +45,23 @@ function addToCart(productId) {
   }
   saveToStorage();
 }
+export function addToCartTest(productId) {
+  let matchingItem;
 
+  cart.forEach((cardItem) => {
+    if (productId === cardItem.productId) {
+      matchingItem = cardItem;
+    }
+  });
+
+  cart.push({
+    productId,
+
+    deliveryOptionId: "1",
+  });
+
+  saveToStorage();
+}
 export function removeFromCart(productId) {
   let newCart = cart.filter((item) => item.productId !== productId);
 
