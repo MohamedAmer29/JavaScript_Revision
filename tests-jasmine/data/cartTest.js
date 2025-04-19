0cf8f076700b711be294b4aa1c62c64336853bb6
Amer <m1o1h1a1a1@gmail.com>
2025-04-19 02:17:04 +0200

    updated test files

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -27,9 +27,9 @@ describe("test suite: addToCartTest", () => {
 
     loadFromStorage();
     addToCartTest("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
-    expect(cart.length).toEqual(3);
+    expect(cart.length).toEqual(2);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-    expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
-    expect(cart[0].quantity).toEqual(2);
+    expect(cart[0].productId).toEqual("15b6fc6f-327a-4ec4-896f-486349e85a3d");
+    expect(cart[0].quantity).toEqual(1);
   });
 });
